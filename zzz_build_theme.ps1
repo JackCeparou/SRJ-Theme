a335079f36b4ab905c1aa95effb8f3c7a5656536
JackCeparou <JackCeparou@users.noreply.github.com>
2019-02-17 16:32:41 +0100

    use buildnumber

--- zzz_build_theme.ps1
+++ zzz_build_theme.ps1
@@ -1,11 +1,17 @@
 $versionFile = Get-Item -Path '.\*.version'
 $versionParts = (Split-Path $versionFile -Leaf).Split(" ")
 
-$version = "$(Get-Date -Format 'yyyy.MM.dd.HHmm').$($versionParts[1])"
-$targetFile = ".\_releases\SRJ.Theme.$version.zip"
+$buildNumber = $Env:BUILD_BUILDNUMBER
+if ($null -eq $buildNumber) { # only exists in the build pipeline
+    $buildNumber = Get-Date -Format 'yyyyMMdd.HHmm'
+}
+
+$v = $versionParts[1].Replace("(", "").Replace(")", "")
+$targetFile = ".\_releases\SRJ.Theme.$buildNumber.$v.zip"
 
 Write-Host $targetFile
 
 Compress-Archive -Path '.\*.version' -DestinationPath $targetFile
+Compress-Archive -Path '.\*.url' -Update -DestinationPath $targetFile
 Compress-Archive -Path '.\Plugins' -Update -DestinationPath $targetFile
 Compress-Archive -Path '.\sounds' -Update -DestinationPath $targetFile
